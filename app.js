014a17e613329d63e1620ff17336b5284216a4f1
saransh-vijayvargiya <vijayvargiyasaransh@gmail.com>
2025-05-26 21:28:48 +0530

    Update app.js

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.get('/', (req, res) => {
-  res.send('Hello from Node.js App deployed via GitHub Actions and Dockers!');
+  res.send('Hello from Node.js App deployed via GitHub Actions and Docker!');
 });
 
 app.listen(PORT, () => {
